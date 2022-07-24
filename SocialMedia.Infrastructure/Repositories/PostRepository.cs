b2e9d64f61aa8ce97b4a1687d38242b4fd0acabe
Brayan Borja Borja <bborja@outlook.com>
2022-07-23 22:33:16 -0500

    Versión 1.0 datos dummies

---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,20 @@
+﻿using SocialMedia.Core.Entities;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post {
+                Id = x,
+                Description = $"Descripcion {x}",
+                Date = DateTime.Now,
+                Image = $"https://misapis.com/{x}",
+                UserId = x * 2,
+            });
+
+            return posts;
+        }
+    }
+}
